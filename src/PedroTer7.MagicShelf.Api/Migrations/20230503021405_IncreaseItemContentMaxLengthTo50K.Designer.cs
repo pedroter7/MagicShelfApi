88335bef08609ee78f5aa79eb7535615859a8bfb
Pedro Tersetti Freidinger <pedrotersetti3@gmail.com>
2023-05-02 23:23:24 -0300

    Update Item entity to accommodate 50k strings in Content property + necessary migrations

--- src/PedroTer7.MagicShelf.Api/Migrations/20230503021405_IncreaseItemContentMaxLengthTo50K.Designer.cs
+++ src/PedroTer7.MagicShelf.Api/Migrations/20230503021405_IncreaseItemContentMaxLengthTo50K.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PedroTer7.MagicShelf.Api.Data.DataContexts;
 
@@ -11,9 +12,11 @@ using PedroTer7.MagicShelf.Api.Data.DataContexts;
 namespace PedroTer7.MagicShelf.Api.Migrations
 {
     [DbContext(typeof(MagicShelfDbContext))]
-    partial class MagicShelfDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230503021405_IncreaseItemContentMaxLengthTo50K")]
+    partial class IncreaseItemContentMaxLengthTo50K
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/PedroTer7.MagicShelf.Api/Migrations/20230503021405_IncreaseItemContentMaxLengthTo50K.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PedroTer7.MagicShelf.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseItemContentMaxLengthTo50K : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
